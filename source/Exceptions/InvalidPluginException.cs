68cce68ed9b663491f8b7244622923ad22ccf2d4
I-AM-SOLO-GetixPolaris <1172200763@qq.com>
2025-04-15 23:07:08 +0800

    Max Website Server 25.01 Code

---
+++ source/Exceptions/InvalidPluginException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MWServer
+{
+    /// 当插件验证失败时抛出的自定义异常
+    public class InvalidPluginException : Exception
+    {
+        /// 初始化带有错误消息的新实例
+        public InvalidPluginException(string message) : base(message)
+        {
+        }
+    }
+}
